44ba6a98f090a8037d7f5c16fc1ae37b44c6ba93
kumarvermarounak2024 <kumarvermarounak2024@gmail.com>
2025-09-20 10:40:33 +0530

    2nd commit

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ initSocket(server); // 👈 call socket setup here
 
 // === Middleware ===
 app.use(cors({
-  origin: ["http://172.31.30.146:1100", "https://smartdhobi.in", "https://api.smartdhobi.in"],
+  origin: ["http://localhost:1100", "http://172.31.30.146:1100", "https://smartdhobi.in", "https://api.smartdhobi.in"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 }));
